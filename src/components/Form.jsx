859aa945112b68a71fa1fdc2998c23447d052ef7
pratiga <gaitrisanu2468@gmail.com>
2022-09-05 16:46:15 +0545

    todo form
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

---
+++ src/components/Form.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
+    const inputTextHandler = (e) => {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+    const submitTodoHandler= (e) =>{
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            { text: inputText, completed:false, id: Math.random()*1000},
+        ]);
+        setInputText("");
+    };
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
+    }
+  return (
+   <form>
+    <input  type="text" className="todo-input"
+     onChange={inputTextHandler}
+     value={inputText}
+      />
+    <button onClick={submitTodoHandler} type="submit" className='todo-button'>
+        <i className='fas fa-plus-square'></i>
+    </button>
+    <div className='select'>
+        <select onChange={statusHandler} name="todos" className='filter-todo'>
+            <option value="all">All</option>
+            <option value="completed">completed</option>
+            <option value="uncompleted">uncompleted</option>
+        </select>
+    </div>
+   </form>
+  )
+}
+
+export default Form
